ba00245c9234deb12b5ddf8b629271c426fc3460
Llsskk <92019037+Llsskk@users.noreply.github.com>
2022-05-28 11:19:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,141 +1,170 @@
-# -*- coding: utf8 -*-
-import json
+# This Python file uses the following encoding: utf-8
+import os
 
-import sqlalchemy
-from flask import jsonify
-from flask_restful import Resource, abort
-from sqlalchemy import *
-from sqlalchemy import MetaData
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, scoped_session
-from flask_marshmallow import Marshmallow
+from flask import Flask, jsonify, request
+from flask_ngrok import run_with_ngrok
+from flask_restful import Api, Resource, reqparse
+from blogs import engine, blogs, session, get_authors, auhtor, get_blogs_, comments, \
+    get_comment
 
-metadata = MetaData()
-DB_URL = 'mysql+pymysql://root:2243@127.0.0.1:3306/blogdb?charset=utf8'
-engine = create_engine(DB_URL)
+# ПОДКЛЮЧАЕМ БИБЛИОТЕКИ И ЗАВИСИМОСТИ
 
-# ПОДКЛЮЧЕНИЕ К ДБ MYSQL
-
-def start() -> scoped_session:
-    base.metadata.bind = engine
-    base.metadata.create_all(engine)
-    return scoped_session(sessionmaker(bind=engine, autoflush=False))
+app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
+api = Api(app)
+run_with_ngrok(app)
 
 
-base = declarative_base()
-session = start()
-
-# КЛАСС БЛОГА
-class blogs(base):
-    '''The model, a plain, ol python cl ass'''
-    __tablename__ = 'blogs'
-    BlogID = Column(Integer, primary_key=True, autoincrement=True)
-    Author = Column(VARCHAR(95), nullable=False)
-    Title = Column(VARCHAR(45), nullable=False)
-    Content = Column(VARCHAR(245), nullable=False)
-    DatePost = Column(DATETIME(), nullable=False)
-
-    def __init__(self, Author, Title, Content, DatePost):
-        self.Author = Author
-        self.Title = Title
-        self.Content = Content
-        self.DatePost = DatePost
-
-    def __repr__(self):
-        return "<blogs(%r, %r, %r, %r)>" % (self.Author, self.Title, self.Content, self.DatePost)
-
-    def make_dict(self):
-        return {'Author': self.Author, 'Title': self.Title, 'Content': self.Content, 'DatePost': self.DatePost}
+# ПОДКЛЮЧАЕМ NGROK СЕРВЕР
 
 
-def abort_if_news_not_found(BlogID):
-    news = session.query(blogs).get(BlogID)
-    if not news:
-        abort(404, message=f"blogs {BlogID} not found")
+@app.route("/")
+def hello():
+    return "Hello server"
 
 
-def get_blogs_():
-    conns = engine.connect()
-    with conns as conn:
-        cur = conn.execute('SELECT * FROM blogs ORDER BY BlogID DESC')
-        posts = cur.fetchall()
-    return posts
-
-# КЛАСС АВТОРОВ
-class auhtor(base):
-    __tablename__ = 'auhtor'
-    AuthorID = Column(Integer, primary_key=True, autoincrement=True)
-    FirstName = Column(VARCHAR(45), nullable=False)
-    LastName = Column(VARCHAR(45), nullable=False)
-    Email = Column(VARCHAR(45), nullable=False)
-    Phone = Column(VARCHAR(12), nullable=False)
-    DateRegistration = Column(DATETIME(), nullable=False)
-
-    def __init__(self, FirstName, LastName, Email, Phone, DateRegistration):
-        self.FirstName = FirstName
-        self.LastName = LastName
-        self.Email = Email
-        self.Phone = Phone
-        self.DateRegistration = DateRegistration
-
-    def __repr__(self):
-        return "<auhtor(%r, %r, %r, %r, %r)>" % (self.FirstName, self.LastName, self.Email, self.Phone, self.DateRegistration)
-
-    def make_dict(self):
-        return {'FirstName': self.FirstName, 'LastName': self.LastName, 'Email': self.Email,
-                'Phone': self.Phone, 'DateRegistration': self.DateRegistration}
-
-def get_authors():
-    conns = engine.connect()
-    with conns as conn:
-        cur = conn.execute('SELECT * FROM auhtor ORDER BY AuthorID DESC')
-        posts = cur.fetchall()
-        # print (json.dumps(posts))
-    return posts
+# ГЛАВНАЯ СТРАНИЦА
 
 
-# КЛАСС КОММЕНТАРИЕВ
-class comments(base):
-    __tablename__ = 'comments'
-    CommentID = Column(Integer, primary_key=True, autoincrement=True)
-    BlogID = Column(Integer, nullable=False)
-    Author = Column(VARCHAR(95), nullable=False)
-    Content = Column(VARCHAR(100), nullable=False)
-    DateComment = Column(DATETIME(), nullable=False)
+class BLOGS(Resource):
 
-    def __init__(self, BlogID, Author, Content, DateComment):
-        self.BlogID = BlogID
-        self.Author = Author
-        self.Content = Content
-        self.DateComment = DateComment
+    def get(self):
+        posts = get_blogs_()
+        results = [dict(row) for row in posts]
+        return jsonify({'results': [dict(row) for row in posts]})
 
-    def __repr__(self):
-        return "<comments(%r, %r, %r, %r)>" % (self.BlogID, self.Author, self.Content, self.DateComment)
+    def post(self):
+        data = request.get_json()
+        for blog_s in data["blogs"]:
+            data2 = blogs(blog_s['Author'], blog_s['Title'], blog_s['Content'], blog_s['DatePost'])
+            session.add(data2)
+        session.commit()
 
-    def make_dict(self):
-        return {'BlogID': self.BlogID, 'Author': self.Author, 'Content': self.Content,
-                'DateComment': self.DateComment}
+    def put(self, BlogID):
+        data_ = request.get_json()
+        data = session.query(blogs).get(BlogID)
+        for blog_s in data_["blogs"]:
+            data.Author = blog_s['Author']
+            data.Title = blog_s['Title']
+            data.Content = blog_s['Content']
+            data.DatePost = blog_s['DatePost']
+        session.commit()
 
-def get_comment():
-    conns = engine.connect()
-    with conns as conn:
-        cur = conn.execute('SELECT * FROM comments ORDER BY CommentID DESC')
-        posts = cur.fetchall()
-        # print (json.dumps(posts))
-    # base.metadata.create_all()
-    return posts
 
-def add_data():
-    data2 = blogs(Author='Мария2', Title="Фокин323а", Content='r2ita@mail.ru', DatePost='2021-05-04 21:25:45')
-    session.add_all([data2])
-    session.commit()
+# КЛАСС С РЕДАКТИРОВАНИЕМ БЛОГА
 
-def bl_delete():
-    data = 'редактирование 28'
-    conns = engine.connect()
-    with conns as conn:
-        cur = conn.execute("SELECT Title, Content FROM blogs WHERE Title = '%s'" % data)
-        posts = cur.fetchall()
-        data_2 =posts
-    print(data_2)
-    return "Guide was successfully deleted"
+class AUTHORS(Resource):
+
+    def get(self):
+        posts = get_authors()
+        return jsonify({'results': [dict(row) for row in posts]})
+
+    def post(self):
+        data_2 = request.get_json()
+        for auhtor_s in data_2["auhtor"]:
+            data2 = auhtor(auhtor_s['FirstName'], auhtor_s['LastName'], auhtor_s['Email'],
+                           auhtor_s['Phone'], auhtor_s['DateRegistration'])
+            session.add(data2)
+        session.commit()
+
+    def put(self, AuthorID):
+        data_ = request.get_json()
+        data = session.query(auhtor).get(AuthorID)
+        for blog_s in data_["auhtor"]:
+            data.Author = blog_s['Author']
+            data.Title = blog_s['Title']
+            data.Content = blog_s['Content']
+            data.DatePost = blog_s['DatePost']
+        session.commit()
+
+
+# КЛАСС С РЕДАКТИРОВАНИЕМ АВТОРОВ
+
+
+class BLOGSsearch(Resource):
+
+    def get(self):
+        data = request.form['name']
+        # data = request.get_data(as_text=True)
+        conns = engine.connect()
+        with conns as conn:
+            cur = conn.execute("SELECT Title, Content FROM blogs WHERE Title = '%s' " % data)
+            posts = cur.fetchall()
+        # results = [dict(row) for row in posts]
+        return jsonify({'results': [dict(row) for row in posts]})
+
+
+api.add_resource(BLOGSsearch, '/bl_search')
+
+
+# ПОИСК БЛОГА
+class AUTHsearch(Resource):
+
+    def get(self):
+        data = request.form['mail']
+        # data = request.get_data(as_text=True)
+        conns = engine.connect()
+        with conns as conn:
+            cur = conn.execute("SELECT FirstName, LastName FROM auhtor WHERE Email = '%s'" % data)
+            posts = cur.fetchall()
+        # results = [dict(row) for row in posts]
+        return jsonify({'results': [dict(row) for row in posts]})
+
+
+api.add_resource(AUTHsearch, '/au_search')
+
+
+# ПОИСК АВТОРА
+class comsearch(Resource):
+
+    def get(self):
+        data = request.form['author']
+        # data = request.get_data(as_text=True)
+        conns = engine.connect()
+        with conns as conn:
+            cur = conn.execute("SELECT FirstName, LastName FROM comments WHERE Author = '%s'" % data)
+            posts = cur.fetchone()
+        # results = [dict(row) for row in posts]
+        return jsonify({'results': [dict(row) for row in posts]})
+
+
+api.add_resource(comsearch, '/co_search')
+
+
+# ПОИСК КОММЕНТАРИЯ
+class COMMENTS(Resource):
+
+    def get(self):
+        posts = get_comment()
+        return jsonify({'results': [dict(row) for row in posts]})
+
+    def post(self):
+        data_2 = request.get_json()
+        for com_s in data_2["comments"]:
+            data2 = comments(com_s['BlogID'], com_s['Author'], com_s['Content'], com_s['DateComment'])
+            session.add(data2)
+        session.commit()
+
+    def put(self, CommentID):
+        data_ = request.get_json()
+        data = session.query(comments).get(CommentID)
+        for blog_s in data_["comments"]:
+            data.Author = blog_s['Author']
+            data.Title = blog_s['Title']
+            data.Content = blog_s['Content']
+            data.DatePost = blog_s['DatePost']
+        session.commit()
+
+
+# КЛАСС С РЕДАКТИРОВАНИЕМ КОММЕНТАРИЯ
+
+api.add_resource(BLOGS, '/bl', '/bl', '/bl_put/<BlogID>')
+api.add_resource(AUTHORS, '/au', '/au', '/au_put/<AuthorID>')
+api.add_resource(COMMENTS, '/co', '/co', '/co_put/<CommentID>')
+# ДОБАВЛЯЕМ КЛАССЫ ДЛЯ ВЫЗОВА API МЕТОДОВ
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    # БЕРЕТ ХОСТ ДЛЯ ПОДКЛЮЧЕНИЯ NGROK
+    app.run()
+
